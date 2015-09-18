d4133e1cdea51481c50d4dcd00ad227110901591
bbroderick <bribroder@gmail.com>
2015-09-18 09:49:08 -0400

    PLERGUNS

--- mksalt.sh
+++ mksalt.sh
@@ -1,2 +1,2 @@
-SALT=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/ 2>&1)
-echo -e "<?php\n$SALT" > wp-salt.php
+echo -e "<?php\n$(curl -s https://api.wordpress.org/secret-key/1.1/salt/ 2>&1)" > wp-salt.php
+chmod 664 wp-salt.php
